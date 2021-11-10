71dda58b69fb73b4760a36491e1304e1b08defb2
sajjadchahardoli <80485775+sajjadchahardoli@users.noreply.github.com>
2021-11-10 16:28:52 +0330

    Register navigation menu

--- functions.php
+++ functions.php
@@ -14,3 +14,11 @@ function bamrahan_register_styles(){
 	wp_enqueue_style('tailwind', get_template_directory_uri() . '/assets/css/tailwind.css', array(), 'all');
 }
 add_action('wp_enqueue_scripts', 'bamrahan_register_styles');
+
+
+// Register a navigation menu location for a theme
+function bamrahan_register_nav_menu(){
+	register_nav_menu('primary','primary Header Navigation Menu');
+	register_nav_menu('secondary','secondary Header Navigation Menu');
+}
+add_action('after_setup_theme','bamrahan_register_nav_menu');
