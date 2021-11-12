65c4967d67dcb8acc313ffe37cfd51414bf9f17b
sajjadchahardoli <80485775+sajjadchahardoli@users.noreply.github.com>
2021-11-12 22:37:52 +0330

    Update profile in header

--- header.php
+++ header.php
@@ -21,15 +21,21 @@
                     <div class="ml-4">
                         <?php echo get_avatar(get_current_user_id(), 32, '', '', array('class' => 'rounded-full')) ?>
                     </div>
-                    <div class="right-">
+                    <div class="">
                         <?php
                         global $current_user;
-                        wp_get_current_user();
-                        echo $current_user->user_login;
+                        if ($current_user->exists()) {
+                            wp_get_current_user();
+                            echo $current_user->user_login;
+                        } else {
+                        ?>
+                            <a href="#" >ورود</a>
+                        <?php
+                        }
                         ?>
                     </div>
-                    <div>
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <div class="border-l-2 pl-6 mr-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
                         </svg>
                     </div>
