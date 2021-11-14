6da70482f4a4bf613f2dfc355f841c5dd9546bca
sajjadchahardoli <80485775+sajjadchahardoli@users.noreply.github.com>
2021-11-14 15:01:38 +0330

    Edit profile and secondray nav

--- header.php
+++ header.php
@@ -12,11 +12,10 @@
     <?php wp_head() ?>
 </head>
 
-<body <?php body_class() ?> dir="rtl">
+<body <?php body_class() ?> dir="rtl" class="box-border">
     <header class="w-full h-16 bg-white shadow">
         <div class="flex justify-between items-center px-6">
             <div class="flex">
-
                 <!--Start navbar brand -->
                 <div class="flex h-16 items-center">
                     <?php if (function_exists('the_custom_logo')) {
@@ -36,47 +35,76 @@
                     )) ?>
                 </nav>
                 <!-- End navigation  -->
-
             </div>
 
-
-
-
+            <!-- Start left navbar -->
             <div class="flex items-center">
-                <div class="ml-4">
-                    <?php echo get_avatar(get_current_user_id(), 32, '', '', array('class' => 'rounded-full')) ?>
+                <!-- Search icon -->
+                <div class=" text-white  ml-4 cursor-pointer relative" onclick="">
+                    <input type="text" class="bg-indigo-100 text-black outline-none w-72 py-2 px-8  hidden rounded-md " id="search-input">
+                    <span class="absolute right-2 top-2 hidden" id="close-search">
+                        <i class="fas fa-times text-black" onclick="closeSearch()"></i>
+                    </span>
+                    <i class="fas fa-search bg-indigo-200 hover:bg-indigo-400 p-3 rounded-md" onclick="openSearch()"></i>
                 </div>
-                <div class="h-full">
+                <!-- Search icon -->
+
+
+                <div class="flex h-full">
                     <?php
                     global $current_user;
                     if ($current_user->exists()) {
-                        wp_get_current_user();
-                        echo $current_user->user_login;
+                    ?>
+                        <div class="flex bg-blue-700 text-white py-2 pr-2 rounded-md">
+                            <?php
+                            echo get_avatar(get_current_user_id(), 24, '', '', array('class' => 'rounded-full'));
+                            wp_get_current_user();
+                            ?>
+                            <div class="cursor-pointer mr-2 select-none " onclick="secondaryNavToggle()">
+                                <?php
+                                echo $current_user->user_login;
+                                ?>
+                                <!-- Arrow bottom icon -->
+                                <span class="bg-indigo-200 text-center rounded-md p-3 mr-2 relative cursor-pointer hover:bg-indigo-400  ">
+                                    <i class="fas fa-ellipsis-v"></i>
+                                    <div class="h-4 w-4 bg-red-600 text-white text-xs rounded-full absolute -left-1 -bottom-1">3</div>
+                                </span>
+                                <!-- Arrow bottom icon -->
+                            </div>
+                        </div>
+
+
+
+                    <?php
                     } else {
                     ?>
-                        <a href="#">ورود</a>
+                        <div class="bg-blue-700 text-white py-2 pr-2 rounded-md ">
+                            <a href="#" class="">ورود / ثبت نام</a>
+                            <span class="bg-indigo-200 text-center rounded-md p-3 mr-2 relative cursor-pointer hover:bg-indigo-400" onclick="secondaryNavToggle()">
+                                <i class="fas fa-ellipsis-v"></i>
+                                <div class="h-4 w-4 bg-red-600 text-white text-xs rounded-full absolute -left-1 -bottom-1">3</div>
+                            </span>
+                        </div>
+
                     <?php
                     }
                     ?>
                 </div>
-                <div class="border-l-2 pl-6 mr-1 cursor-pointer text-gray-500 hover:text-black">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
-                    </svg>
-                </div>
-                <div class="">
+
+
+
+                <!-- Secondary navigation-->
+                <div class="hidden" id="secondary-nav">
                     <?php wp_nav_menu(array(
                         'theme_location' => 'secondary',
                         'container' => false,
-                        'menu_class' => 'flex flex-col justify-center mx-5 absolute top-20 right-0 shadow rounded-md p-4'
+                        'menu_class' => 'flex flex-col justify-center mx-5 absolute top-20 left-0 shadow rounded-md p-4'
                     )) ?>
                 </div>
-                <span>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                    </svg>
-                </span>
+                <!-- Secondary navigation-->
+
             </div>
+            <!-- End left navbar -->
         </div>
 
 
