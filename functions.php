f704c0bb85d626a77da6c837f07fde704326599b
sajjadchahardoli <80485775+sajjadchahardoli@users.noreply.github.com>
2021-11-14 10:45:18 +0330

    Add font awesome

--- functions.php
+++ functions.php
@@ -12,6 +12,7 @@ add_action('after_setup_theme', 'bamrahan_setup');
 
 function bamrahan_register_styles(){
 	wp_enqueue_style('tailwind', get_template_directory_uri() . '/assets/css/tailwind.css', array(), 'all');
+    wp_enqueue_style('font-awesome','https://pro.fontawesome.com/releases/v5.10.0/css/all.css', array(), 'all');
 }
 add_action('wp_enqueue_scripts', 'bamrahan_register_styles');
 
