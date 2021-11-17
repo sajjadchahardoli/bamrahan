e0e5cca9387cabd3510b340bfd8277f234dce07e
sajjadchahardoli <80485775+sajjadchahardoli@users.noreply.github.com>
2021-11-17 10:06:55 +0330

    Check have post and show

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
-<?php
-get_header();
-get_footer();
+<?php get_header(); ?>
+
+<main>
+    <div>
+        <?php
+        if (have_posts()) {
+            while (have_posts()) {
+                the_post();
+                get_template_part('template-parts/content/content', get_post_format());
+            }
+        }
+        ?>
+    </div>
+</main>
+
+<?php get_footer(); ?>
