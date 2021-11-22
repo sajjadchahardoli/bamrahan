6124c1c2e8818ba2ebae87d4ad861d84ae52bf8c
sajjadchahardoli <80485775+sajjadchahardoli@users.noreply.github.com>
2021-11-22 20:31:46 +0330

    Add about section in main page

--- index.php
+++ index.php
@@ -1,10 +1,23 @@
 <?php get_header(); ?>
 
 <main>
+
     <div>
         <?php
         echo do_shortcode('[smartslider3 slider="1"]');
         ?>
+        <div class="w-11/12 shadow rounded-md my-5 mx-auto px-2 pt-2 pb-40">
+            <h2>درباره ما</h2>
+            <p>
+                توضیحات درباره ما
+            </p>
+            <p>
+                توضیحات درباره ما
+            </p>
+            <p>
+                توضیحات درباره ما
+            </p>
+        </div>
         <?php
         if (have_posts()) {
             while (have_posts()) {
@@ -14,6 +27,7 @@
         }
         ?>
     </div>
+
 </main>
 
 <?php get_footer(); ?>
