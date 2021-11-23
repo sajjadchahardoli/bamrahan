7953dcb704543b63ddf9c8e1a6c3cab552310570
sajjadchahardoli <80485775+sajjadchahardoli@users.noreply.github.com>
2021-11-23 23:29:18 +0330

    Add footer about section header

--- footer.php
+++ footer.php
@@ -1,3 +1,26 @@
+        <footer>
+            <div class="flex">
+                <div>
+                    <div class="flex">
+                        <div>
+                            <?php if (function_exists('the_custom_logo')) {
+                                the_custom_logo();
+                            }
+                            ?>
+                        </div>
+                        <div>
+                            <?php bloginfo('name'); ?>
+                        </div>
+
+
+                    </div>
+                    <div></div>
+                </div>
+                <div></div>
+                <div></div>
+            </div>
+        </footer>
         <?php wp_footer(); ?>
-    </body>
-</html>
+        </body>
+
+        </html>
